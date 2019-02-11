107670d3ecdf53b5e1860f1b863d72ade0084ff5
Raja Mukherji <rajamukherji@gmail.com>
2019-02-11 06:25:06 +0000

    Initial formula.

---
+++ rabs.rb
@@ -0,0 +1,14 @@
+class Rabs < Formula
+	desc "General purpose imperative build system"
+	homepage "https://wrapl.github.io/tools/rabs.xhtml"
+	url "https://github.com/wrapl/rabs.git", :tag => "v1.6.2"
+	depends_on "bdw-gc" => build
+
+	def install
+		system "make", "install", "PREFIX=#{prefix}"
+	end
+
+	test do
+		system "#{bin}/rabs", "-v"
+	end
+end
