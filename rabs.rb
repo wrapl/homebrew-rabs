cc03c731058ae3adc3e673ed395c477803fec7db
Raja Mukherji <rajamukherji@gmail.com>
2020-04-15 17:02:55 +0100

    Update rabs.rb

--- rabs.rb
+++ rabs.rb
@@ -1,7 +1,7 @@
 class Rabs < Formula
 	desc "General purpose imperative build system"
 	homepage "https://wrapl.github.io/tools/rabs.xhtml"
-	url "https://github.com/wrapl/rabs.git", :tag => "v2.6.1"
+	url "https://github.com/wrapl/rabs.git", :tag => "v2.8.1"
 	depends_on "bdw-gc"
 
 	def install
